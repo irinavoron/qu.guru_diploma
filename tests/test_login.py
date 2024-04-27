851383082467f55ce75caefc66a98079b67360c3
Irina.Voron <yarutich.irina@gmail.com>
2024-04-27 21:19:39 +0300

    Add first test

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,11 @@
-from selene import browser
+from selene import browser, be
 
 
 def test_standard_user_login():
     browser.open('/')
+
+    browser.element('#user-name').type('standard_user')
+    browser.element('#password').type('secret_sauce')
+    browser.element('#login-button').click()
+
+    browser.element('.inventory_list').should(be.visible)
