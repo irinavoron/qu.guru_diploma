9b6a72c3b384559ef174f23d1e9ee30cb4826158
Irina.Voron <yarutich.irina@gmail.com>
2024-05-01 14:14:31 +0300

    Moved tests to login.py

--- tests/test_saucedemo.py
+++ tests/test_saucedemo.py
@@ -3,7 +3,6 @@ import os
 import allure
 from allure_commons.types import Severity
 from selene import browser, be, have
-import pytest
 from dotenv import load_dotenv
 
 from saucedemo_test.data import products
@@ -18,41 +17,6 @@ cart = Cart()
 load_dotenv()
 user_name = os.getenv('USER_NAME')
 user_password = os.getenv('USER_PASSWORD')
-
-
-@allure.title('Successful login')
-@allure.tag('web', 'smoke')
-@allure.story('User can login with valid creds')
-@allure.feature('Login')
-@allure.label('owner', 'irinaV')
-@allure.severity(Severity.BLOCKER)
-def test_successful_login():
-    login_page.successful_login()
-
-    with allure.step('The catalogue is opened after logging in'):
-        browser.element('.inventory_list').should(be.visible)
-
-
-@allure.title('Unsuccessful login')
-@allure.tag('web')
-@allure.story('User can`t login with invalid creds')
-@allure.feature('Login')
-@allure.label('owner', 'irinaV')
-@allure.severity(Severity.CRITICAL)
-@pytest.mark.parametrize(
-    'login, password', [
-        ('wrong_name', {user_password}),
-        ({user_name}, 'wrong_password'),
-        ('', ''),
-        ('locked_out_user', {user_password})
-    ],
-    ids=['invalid name', 'invalid password', 'empty form', 'locked out user']
-)
-def test_unsuccessful_login(login, password):
-    login_page.unsuccessful_login(login, password)
-
-    with allure.step('Verify that error message is displayed'):
-        browser.element('.error-button').should(be.visible)
 
 
 @allure.title('Cart badge displays items number')
